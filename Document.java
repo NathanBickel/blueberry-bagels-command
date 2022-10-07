ee80719f5156342fe1ce99e3c75905fa0bb458d5
jrginn <jrginn47@gmail.com>
2022-10-07 18:18:45 -0400

    Document stubs

---
+++ Document.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+// Jackson Ginn
+
+public class Document {
+    private String fileName;
+    private ArrayList<String> lines;
+
+    public Document(String name){
+
+    }
+
+    public String view(){
+
+    }
+
+    public String append(String line){
+
+    }
+
+    public String write(String line){
+
+    }
+
+    public String save(){
+        
+    }
+}
