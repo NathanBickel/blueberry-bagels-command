038b0c9446fe86e1ed061012bb52df90f297336e
NathanBickel <nbickel@email.sc.edu>
2022-10-07 18:17:43 -0400

    Implemented InputHandler and Command

--- Command.java
+++ Command.java
@@ -1,11 +1,21 @@
+/**
+ * @author Nathan Bickel
+ */
 public abstract class Command {
     
     protected Document document;
 
+    /**
+     * Constructor for Command
+     * @param document document taken in
+     */
     public Command(Document document) {
-
+        this.document = document;
     }
 
+    /**
+     * @return String from code executing
+     */
     public abstract String execute();
 
 }
--- InputHandler.java
+++ InputHandler.java
@@ -1,15 +1,30 @@
 import java.util.HashMap;
 
+/**
+ * @author Nathan Bickel
+ */
 public class InputHandler {
     
     private HashMap<String, Command> commands;
 
+    /**
+     * Constructor for InputHandler
+     * @param document Document given
+     */
     public InputHandler(Document document) {
-        
+        commands = new HashMap<String, Command>();
+        commands.put("view", new ViewCommand(document));
+        commands.put("append", new AppendCommand(document));
+        commands.put("write", new WriteCommand(document));
+        commands.put("save", new SaveCommand(document));
     }
 
+    /**
+     * Calls Command from command string
+     * @param data Command that maps to HashMap
+     */
     public void inputEntered(String data) {
-
+        commands.get(data).execute();
     } 
 
 }
