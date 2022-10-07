0ded19066c22a38337cd73e371ccf65cadb6cf9c
AnHonestLawyer <chancestorey@gmail.com>
2022-10-07 18:43:43 -0400

    Added Document.view() code for ViewCommand

--- Document.java
+++ Document.java
@@ -13,7 +13,25 @@ public class Document {
     }
 
     public String view(){
-
+        //Determine max line length for border
+        int len = 8;
+        for(String line : lines){
+            // Adds extra 10 spacing for decoration
+            if(line.length() + 10 > len){
+                len = line.length() + 10;
+            }
+        }
+        //Print top border
+        for(int i = 0; i < len; i++)
+            System.out.print("_");
+        System.out.println();
+        //Print each line decorated
+        for(String line : lines)
+            System.out.println("|    "+line+"    |");
+        //Print bottom border
+        for(int i = 0; i < len; i++)
+            System.out.print("_");
+        System.out.println();
     }
 
     public String append(String line){
