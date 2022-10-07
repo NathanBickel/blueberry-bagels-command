a08356eedf1cccb2d47bd8794c5402a52e7ab2ee
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2022-10-07 18:55:24 -0400

    finished write command

--- WriteCommand.java
+++ WriteCommand.java
@@ -3,11 +3,20 @@ import java.util.*;
  * @author Katelyn Wyandt
  */
 public class WriteCommand extends Command{
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner in = new Scanner(System.in);
+    /**
+     * Calls super class to write line to document
+     * @param doc document being manipulated by child class
+     */
     public WriteCommand(Document doc){
         super(doc);
     }
+    /**
+     * Calls the write method of the super class
+     * @return document.write(string)
+     */
     public String execute(){
-        return document.write(Scanner.in);
+        String string = in.next();
+        return document.write(string);
     }
 }
