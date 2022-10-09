1b9d4f09728c75845de8483b25a2a55240fd0713
jrginn <jrginn47@gmail.com>
2022-10-09 14:21:58 -0400

    added JavaDoc to SaveCommand

--- SaveCommand.java
+++ SaveCommand.java
@@ -7,10 +7,17 @@
   */
 public class SaveCommand extends Command{
     
+    /***
+     * Creates a SaveCommand object with the specified Document
+     * @param doc Document to be saved
+     */
     public SaveCommand(Document doc){
         super(doc);
     }
 
+    /***
+     * Calls the document's save method and returns its output
+     */
     public String execute(){
         return document.save();
     }
